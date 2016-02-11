42892c5bec5da4b4e4a9fa202a4fc031ea09938e
Elena Filenko <elena.filenko@blitit.com>
2016-02-10 19:58:38 -0800

    MErors fixed

--- copy_frames.sh
+++ copy_frames.sh
@@ -15,6 +15,29 @@ cp -R . ../Cura
 cd ../Uranium_frame
 rm -rf .git
 cp -R . ../Uranium
+cd ..
+
+
+#Remove Cura_frame and Uranium_frame
+rm -rf Cura_frame
+rm -rf Uranium_frame
 
 #Restore git folders
 ./get_frames_filushka.sh
+
+#Rename original CMakeLists.txt file in Cura
+pwd
+cd Cura 
+pwd
+mv CMakeLists.txt CMakeLists_orig.txt
+mv CMakeLists_my.txt CMakeLists.txt
+cd ..
+
+#Rename original CMakeLists.txt file in Uranium
+pwd
+cd Uranium
+pwd
+mv CMakeLists.txt CMakeLists_orig.txt
+mv CMakeLists_my.txt CMakeLists.txt
+cd ..
+
